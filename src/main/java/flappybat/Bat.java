888d06b06defaee90501d4397e21f0ef287ab7e0
p-an-na <an.pietruszka@gmail.com>
2020-06-16 16:05:19 +0200

    Dodanie podstaw do klas

--- src/main/java/flappybat/Bat.java
+++ src/main/java/flappybat/Bat.java
@@ -1,5 +1,49 @@
 package flappybat;
+//porusza siê do góry, przy naciœniêciu na niego plansza siê przesuwa do przodu i wyskakuj¹ kolejne przeszkody. Nie naciskamy myszk¹ spada w dó³
+public class Bat implements IMove {
+	private String name;
+	private int Xaxis;
+	private int Yaxis;
 
-public class Bat {
-int i;
+	
+	private int dx;
+	private int dy;
+	
+	public Bat(String name, int polozenieNaOsiX, int polozenieNaOsiY) {
+		this.name = name;
+		this.Xaxis = polozenieNaOsiX;
+		this.Yaxis = polozenieNaOsiY;
+		this.dx = 2;
+		this.dy = 2;
+	}
+
+	@Override
+	public String toString() {
+		return "Bat [name=" + name + ", polozenieNaOsiX=" + Xaxis + ", polozenieNaOsiY="
+				+ Yaxis + "]";
+	}
+
+
+	public void move(int units) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void moveUp() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void moveDown() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void position() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
 }
+
--- src/main/java/flappybat/IMove.java
+++ src/main/java/flappybat/IMove.java
@@ -2,4 +2,14 @@ package flappybat;
 
 public interface IMove {
 
-}
+	public void move(int units);
+	public void moveUp();
+	public void moveDown();
+	public void moveStop();
+	public void position();
+	
+	
+	
+	}
+
+// jak klikamy na niego to porusza siê do góry i jednoczenie do przodu? jak nie klikamy to spada w dó³?
